70763e6e210cd85e555e66d3b3fdcffe6b09b0d3
Georges Auberger <georges@ternarylabs.com>
2024-11-25 09:14:32 -1000

    Refactor sending

--- perf-test.rb
+++ perf-test.rb
@@ -1,8 +1,9 @@
 require 'rubyserial'
 
-radio2 = Serial.new('/dev/cu.usbmodem101', 9600, 8, :none) # TH-D75
-#radio2 = Serial.new('/dev/cu.VR-N76', 9600, 8, :none)
-radio1 = Serial.new('/dev/cu.UV-PRO', 9600, 8, :none)
+
+radio1 = Serial.new('/dev/cu.usbmodem1101', 9600, 8, :none) # TH-D75
+radio2 = Serial.new('/dev/cu.VR-N76', 9600, 8, :none)
+#radio1 = Serial.new('/dev/cu.UV-PRO', 9600, 8, :none)
 
 puts "Start receiving data from radio"
 # Start receiving thread
@@ -11,8 +12,11 @@ Thread.new do
         begin
         data = radio2.getbyte
         unless data.nil?
+            if data == 0xC0
+                print "\n"
+            end
             # display hex representation of data formatted as 0x00
-            puts "0x%02X" % data        
+            print "0x%02X," % data        
         end
         rescue Exception => e
         break
@@ -21,12 +25,13 @@ Thread.new do
 end
 
 puts "Sending data to radio"
-radio1.write("\xC0\x00\x01\x01\x01\x01\x01\x01\x01\x01\xC0")
-radio1.write("\xC0\x00\x02\x02\x02\x02\x02\x02\x02\x02\xC0")
-radio1.write("\xC0\x00\x03\x03\x03\x03\x03\x03\x03\x03\xC0")
-radio1.write("\xC0\x00\x04\x04\x04\x04\x04\x04\x04\x04\xC0")
+1..6.times do |i|
+    payload = [0xC0, 0x00] + [i+1] * 16  + [0xC0]
+    print "Sending payload number #{i+1}, written bytes: "
+    puts radio1.write(payload.pack('C*'))
+end
 
-sleep(5)
+sleep(10)
 
 puts "Closing serial ports"
 radio1.close
